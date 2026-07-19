459768267fe2d22110ba53025aaf634a5aee78ae
Shreyas Sonawane <F24ET098@ms.pict.edu>
2026-07-19 10:49:11 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (79.19%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,24 +1,6 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int s = 0;
-        int e = nums.size()-1;
-        int mid = 0;
-
-        while(e >= mid){
-            if(nums[mid] == 2){
-                swap(nums[mid],nums[e]);
-                e--;
-                
-            }
-            else if(nums[mid] == 0){
-                swap(nums[mid], nums[s]);
-                s++;
-                mid++;
-            }
-            else{
-                mid++;
-            }
-        }
+        sort(nums.begin(),nums.end());
     }
 };
