c562852db8f84c5347cb8b0bdf8729e743df0fa8
Shreyas Sonawane <F24ET098@ms.pict.edu>
2026-07-21 19:04:51 +0530

    Time: 2 ms (70.13%) | Memory: 17.3 MB (76.77%) - LeetSync

---
+++ 692-top-k-frequent-words/top-k-frequent-words.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<string> topKFrequent(vector<string>& words, int k) {
+
+        unordered_map<string, int> mp;
+
+        for (auto &x : words) {
+            mp[x]++;
+        }
+
+        vector<pair<string, int>> v(mp.begin(), mp.end());
+
+        sort(v.begin(), v.end(), [](auto &a, auto &b) {
+            if (a.second == b.second)
+                return a.first < b.first;
+            return a.second > b.second;
+        });
+
+        vector<string> ans;
+
+        for (int i = 0; i < k; i++) {
+            ans.push_back(v[i].first);
+        }
+
+        return ans;
+    }
+};
