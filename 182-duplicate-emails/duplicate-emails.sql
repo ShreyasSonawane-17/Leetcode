17bfe80550a5280ce050a56670e4d9fa173cfa6e
Shreyas Sonawane <F24ET098@ms.pict.edu>
2026-08-27 17:04:56 +0530

    Time: 492 ms (18.29%) | Memory: 0B (100.00%) - LeetSync

--- 182-duplicate-emails/duplicate-emails.sql
+++ 182-duplicate-emails/duplicate-emails.sql
@@ -1,6 +1,8 @@
 # Write your MySQL query statement below
-select distinct a.email as Email 
-from Person a
-join Person b
-on a.email = b.email
-where a.id != b.id;
+#select distinct a.email as Email 
+#from Person a
+#join Person b
+#on a.email = b.email
+#where a.id != b.id;
+
+select email from person group by email having count(email) > 1;
