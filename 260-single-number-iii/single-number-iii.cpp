e732a9ae9bab4849e63acdc796a8da8d644c13aa
Shreyas Sonawane <F24ET098@ms.pict.edu>
2026-09-24 19:07:25 +0530

    Time: 3 ms (25.07%) | Memory: 14.7 MB (23.37%) - LeetSync

---
+++ 260-single-number-iii/single-number-iii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        unordered_map<int,int> mp;
+        for(auto x : nums){
+            mp[x]++;
+        }
+        vector<int> ans;
+        for(auto it : mp){
+            if(it.second == 1) ans.push_back(it.first);
+        }
+
+        return ans;
+    }
+};
